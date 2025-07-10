833c4470c0d5853d3df86fab8a1e7fa4a41684ed
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-07-10 19:07:25 -0300

    feat: save directory name image

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+ Produto:
+        Produto:
+            nome - Char
+            descricao_curta - Text
+            descricao_longa - Text
+            imagem - Image
+            slug - Slug
+            preco_marketing - Float
+            preco_marketing_promocional - Float
+            tipo - Choices
+                ('V', 'Variável'),
+                ('S', 'Simples'),
+
+        Variacao:
+            nome - char
+            produto - FK Produto
+            preco - Float
+            preco_promocional - Float
+            estoque - Int
+
+
+"""
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True
+    )
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        print(img.name)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.image, max_image_size)
+
+    def __str__(self):
+        return self.nome
